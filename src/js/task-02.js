9d6d4e789c9dde4a9b541f2d65fc4bce5bc215bc
Yuriy Ofitsynskyy <yuriy.ofitsynskyy@gmail.com>
2023-04-10 21:43:07 +0300

    Task 2 - done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ingredients.forEach(function(ingredient) {
+  const ingredientMarkup = document.createElement("li");
+  ingredientMarkup.textContent = `${ingredient}`;
+  ingredientMarkup.classList.add("item");
+  console.log(ingredientMarkup);
+  const ingredientsList = document.querySelector("#ingredients");
+  ingredientsList.appendChild(ingredientMarkup);
+})
