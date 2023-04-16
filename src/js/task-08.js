ac11d7dc9d6a4edb9ef85c0ab682b0c37dba4d22
Yuriy Ofitsynskyy <yuriy.ofitsynskyy@gmail.com>
2023-04-16 19:43:37 +0300

    Task 8 - done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+const email = event.currentTarget.elements.email.value;
+const password = event.currentTarget.elements.password.value;
+
+if(email === "" || password === "") {
+    return alert("Усі поля повинні бути заповненими!")
+}
+
+const formData = {
+    email,
+    password,
+};
+console.log(formData);
+event.currentTarget.reset();
+})
